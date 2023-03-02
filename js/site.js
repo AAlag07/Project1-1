410674b402b6f72b2074e6512121354f46ec9e61
adrianhamel <ahamel@hawk.iit.edu>
2023-03-02 15:23:37 -0600

    Added to the styling and added more messages

--- js/site.js
+++ js/site.js
@@ -6,6 +6,8 @@ var timeNow = + currentdate.getHours() + ":"
 
 function on() {
   document.getElementById("overlay").style.display = "block";
-  document.getElementById("overlay-text").innerHTML = "Submitted at: " + timeNow + "\nThank you for submitting\nCheck your email for more information";
+  document.getElementById("overlay-text").innerHTML = "Submitted at: " + timeNow;
+  document.getElementById("overlay-text2").innerHTML = "Glad you could join us"
+  document.getElementById("overlay-text3").innerHTML = "Check your email for more information"
   document.getElementById("form").style.display = "none";
 }
