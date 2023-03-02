f49ed68de6c52a65d40d3f8a312f11d2c2ceee43
adrianhamel <ahamel@hawk.iit.edu>
2023-03-02 13:59:18 -0600

    editing JS

--- js/site.js
+++ js/site.js
@@ -1,13 +1,16 @@
 function time() {
   var currentdate = new Date(); 
+  var message = "Thank you for signing up!" + "\n" + "Check your inbox for more information";
   var timenow = + currentdate.getHours() + ":"  
     + currentdate.getMinutes() + ":" 
     + currentdate.getSeconds();
-  document.getElementById("overlay-text").innerHTML = "Submitted at: " + timenow;
-  }
+  document.getElementById("overlay-text").innerHTML = "Submitted at: " + timenow + message;
+}
+
 
 function on() {
   document.getElementById("overlay").style.display = "block";
+  document.getElementById("form").style.display = "block";
 }
   
 function off() {
