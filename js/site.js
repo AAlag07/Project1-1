853601d9c08fad1911898dd1be21f55ffaa779ed
adrianhamel <ahamel@hawk.iit.edu>
2023-03-02 14:39:56 -0600

    Added Asny to meet requirement

--- js/site.js
+++ js/site.js
@@ -6,6 +6,6 @@ var timeNow = + currentdate.getHours() + ":"
 
 function on() {
   document.getElementById("overlay").style.display = "block";
-  document.getElementById("overlay-text").innerHTML = "Submitted at: " + timeNow;
+  document.getElementById("overlay-text").innerHTML = "Submitted at: " + timeNow + "\nThank you for submitting\nCheck your email for more information";
   document.getElementById("form").style.display = "none";
 }
