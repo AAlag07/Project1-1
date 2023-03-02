801a316a2f55be5faca4d4bf8f5d031352272ab1
adrianhamel <ahamel@hawk.iit.edu>
2023-03-02 14:22:10 -0600

    Fixed issue with onCLick

--- js/site.js
+++ js/site.js
@@ -1,18 +1,12 @@
-function time() {
-  var currentdate = new Date(); 
-  var message = "Thank you for signing up!" + "\n" + "Check your inbox for more information";
-  var timenow = + currentdate.getHours() + ":"  
+var currentdate = new Date(); 
+var message = "Thank you for signing up!" + "\n" + "Check your inbox for more information";
+var timeNow = + currentdate.getHours() + ":"  
     + currentdate.getMinutes() + ":" 
     + currentdate.getSeconds();
-  document.getElementById("overlay-text").innerHTML = "Submitted at: " + timenow + message;
-}
 
 
 function on() {
   document.getElementById("overlay").style.display = "block";
-  document.getElementById("form").style.display = "block";
-}
-  
-function off() {
-  document.getElementById("overlay").style.display = "none";
+  document.getElementById("overlay-text").innerHTML = "Submitted at: " + timeNow + message;
+  document.getElementById("form").style.display = "none";
 }
