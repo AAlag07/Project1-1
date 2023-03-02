8d76759c0546d4b058f9b96c998cd26301a25670
adrianhamel <ahamel@hawk.iit.edu>
2023-03-02 14:33:55 -0600

    Changed messgae

--- js/site.js
+++ js/site.js
@@ -1,5 +1,4 @@
 var currentdate = new Date(); 
-var message = "Thank you for signing up!" + "\n" + "Check your inbox for more information";
 var timeNow = + currentdate.getHours() + ":"  
     + currentdate.getMinutes() + ":" 
     + currentdate.getSeconds();
@@ -7,6 +6,6 @@ var timeNow = + currentdate.getHours() + ":"
 
 function on() {
   document.getElementById("overlay").style.display = "block";
-  document.getElementById("overlay-text").innerHTML = "Submitted at: " + timeNow + message;
+  document.getElementById("overlay-text").innerHTML = "Submitted at: " + timeNow;
   document.getElementById("form").style.display = "none";
 }
