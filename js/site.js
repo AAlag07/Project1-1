f42fe9c345f5562329382a14e94a11dbf5574323
adrianhamel <ahamel@hawk.iit.edu>
2023-03-02 12:50:50 -0600

    Fixed JS and updated CSS

--- js/site.js
+++ js/site.js
@@ -3,12 +3,13 @@ function time() {
   var timenow = + currentdate.getHours() + ":"  
               + currentdate.getMinutes() + ":" 
               + currentdate.getSeconds();
-    document.getElementById("dateButton").innerHTML = "Submitted at: "+timenow;
+    document.getElementById("overlay-text").innerHTML = "Submitted at: " + timenow;
   }
 
   function on() {
     document.getElementById("overlay").style.display = "block";
   }
+  document.getElementById("demo").innerHTML = 5 + 6;
   
   function off() {
     document.getElementById("overlay").style.display = "none";
